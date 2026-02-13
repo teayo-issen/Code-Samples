871964918b66af188c7ca6c5b39637f69bbdea79
teayo-issen <olawrencetheocrissen@gmail.com>
2026-02-13 21:30:36 +0800

    Update Guessgame.java

--- Guessgame.java
+++ Guessgame.java
@@ -4,8 +4,9 @@ class Guessgame{
   static Random N= new Random();
   static int Secret= N.nextInt(100)+1;
   static int Guess=0;
+  static String ans= "";
   public static void main(String[] teao){
-
+do {Secret = N.nextInt(101); Guess=0
     do {
       System.out.print("Enter your Guess(1-100): ");
       Guess= input.nextInt();
@@ -16,5 +17,9 @@ class Guessgame{
         else 
         System.out.print("Congrats!!!!");
     } while (Guess!=Secret);
+     System.out.print("Play again Y/N?: ");
+    ans= input.next();
+   } while (!ans.equalsIgnoreCase("N"));
+     System.out.print("Au revoir");
   }
 }
